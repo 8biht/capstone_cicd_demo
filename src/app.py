3e600a9ddbc73405fb6a7652d63125cd0a88fb87
8biht <brooklynihunt@gmail.com>
2025-12-05 12:49:30 -0600

    added more operations

--- src/app.py
+++ src/app.py
@@ -1,2 +1,37 @@
+import math
+
 def add (a, b):
     return a + b
+
+def subtract (a, b):
+    return a - b
+
+def multiply (a, b):
+    return a * b
+
+def divide (a, b):
+    return a / b
+
+def log (a):
+    return math.log(a)
+
+def square (a):
+    return a * a
+
+def sin (a):
+    return math.sin(a)
+
+def cos (a):
+    return math.cos(a)
+
+def square_root (a):
+    if a / abs(a) == -1:
+        print("Cannot take the square root of a negative number.")
+        break
+    return math.sqrt(a)
+
+def percentage (a, b):
+    if a / abs(a) == -1:
+        print("Cannot have a negative percentage.")
+        break
+    return (a / b ) * 100
